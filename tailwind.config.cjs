c8761b990b2580d6ab98839b63e4bf3ce06c746d
Taneeya <2240351540@qq.com>
2023-09-20 19:07:58 +0800

    change bg

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -43,9 +43,9 @@ module.exports = {
       },
       backgroundImage: {
         header: 'url("images/bg-hero.jpg")',
-        bgDesktop: "url('images/bg-hero.jpg'), url('images/bg-footer.jpg')",
-        bgMobile:
-          "url('images/mobile-bg-hero.jpg'), url('images/bg-footer.jpg')",
+        bgDesktop: "url('images/bg-hero.jpg')",
+        bgMobile: "url('images/mobile-bg-hero.jpg')",
+        bgFooter: "url('images/bg-footer.jpg')",
       },
     },
   },
