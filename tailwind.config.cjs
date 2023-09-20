3897da785b0bd27e68920d30f348e5ba69c3bb7a
Taneeya <2240351540@qq.com>
2023-09-20 14:38:56 +0800

    replace all big image

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,7 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: [
     "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
-    "./node_modules/flowbite/**/*.js"
+    "./node_modules/flowbite/**/*.js",
   ],
   theme: {
     screens: {
@@ -18,38 +18,36 @@ module.exports = {
         sans: ["InterVariable", "Inter", ...defaultTheme.fontFamily.sans],
       },
       letterSpacing: {
-        0.65: '0.65px',
-        0.7: '0.7px',
-        0.8: '0.8px',
-        1: '1px',
-        1.2: '1.2px',
-        1.8: '1.8px',
-        2.4: '2.4px',
-        2.6: '2.6px',
-        3: '3px',
-        3.2: '3.2px',
+        0.65: "0.65px",
+        0.7: "0.7px",
+        0.8: "0.8px",
+        1: "1px",
+        1.2: "1.2px",
+        1.8: "1.8px",
+        2.4: "2.4px",
+        2.6: "2.6px",
+        3: "3px",
+        3.2: "3.2px",
       },
       height: {
-        65: '65px',
+        65: "65px",
       },
       colors: {
-        'normal': '#1F2024',
-        'pink': '#8967E7',
-        'gray': '#F9F9F9',
-        'black': '#000000',
+        normal: "#1F2024",
+        pink: "#8967E7",
+        gray: "#F9F9F9",
+        black: "#000000",
       },
       fontSize: {
-        64: '64px',
+        64: "64px",
       },
       backgroundImage: {
-        header: 'url("images/bg-hero.svg")',
-        bgDesktop: "url('images/bg-hero.svg'), url('images/bg-footer.svg')",
-        bgMobile: "url('images/mobile-bg-hero.svg'), url('images/bg-footer.svg')",
-      }
+        header: 'url("images/bg-hero.jpg")',
+        bgDesktop: "url('images/bg-hero.jpg'), url('images/bg-footer.jpg')",
+        bgMobile:
+          "url('images/mobile-bg-hero.jpg'), url('images/bg-footer.jpg')",
+      },
     },
   },
-  plugins: [
-    require("@tailwindcss/typography"),
-    require('flowbite/plugin')
-  ],
+  plugins: [require("@tailwindcss/typography"), require("flowbite/plugin")],
 };
