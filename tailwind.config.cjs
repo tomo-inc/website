67b328ea9f9b618117927448602e198f2a736ad5
Sophie <sophie4140@outlook.com>
2023-12-06 14:02:51 +0800

    improve-ui

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -39,9 +39,9 @@ module.exports = {
         64: "64px",
       },
       backgroundImage: {
-        bgDesktop: "url('/images/bg-body.jpg')",
-        bgMobile:
-          "url('/images/mobile-bg-hero.jpg'), url('/images/bg-footer.jpg')",
+        // bgDesktop: "url('/images/bg-body.jpg')",
+        // bgMobile:
+        //   "url('/images/mobile-bg-hero.jpg'), url('/images/bg-footer.jpg')",
       },
     },
   },
