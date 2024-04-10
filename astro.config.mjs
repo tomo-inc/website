59499386f88e50994a69a38b2fd461b48da69af8
Sophie <sophie4140@outlook.com>
2024-04-11 00:24:08 +0800

    bugfix

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,9 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  devToolbar: {
+    enabled: false,
+  },
   integrations: [
     tailwind(),
     image({
