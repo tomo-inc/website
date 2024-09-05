a71e9ec6a2aa3bcd8908d6dea65fd09a22d70614
Sophie <sophie4140@outlook.com>
2024-09-05 17:23:48 +0800

    bugfix

--- src/components/react/carousel.tsx
+++ src/components/react/carousel.tsx
@@ -24,32 +24,3 @@ const Carousel = () => {
 };
 
 export default Carousel;
-
-// <div className="scroll-container w-[300px] md:w-[400px] h-[700px] md:h-[900px]">
-//   <div className="scroll-content gap-3 md:gap-8">
-//     {images.concat(images).map((image, index) => (
-//       <div key={index} className="scroll-item">
-//         <img
-//           src={image}
-//           alt={`Scrolling content ${index}`}
-//           className="scroll-image"
-//         />
-//       </div>
-//     ))}
-//   </div>
-//   <div
-//     className=" w-[300px] md:w-[400px] h-[186px] md:h-[300px] absolute z-20 top-0"
-//     style={{
-//       backgroundImage:
-//         "linear-gradient(360deg, rgba(18, 18, 18, 0.00) 0%, #121212 88.49%)",
-//     }}
-//   ></div>
-
-//   <div
-//     className=" w-[300px] md:w-[400px] h-[186px] md:h-[300px] absolute z-20 bottom-0"
-//     style={{
-//       backgroundImage:
-//         "linear-gradient(180deg, rgba(18, 18, 18, 0.00) 0%, #121212 88.49%)",
-//     }}
-//   ></div>
-// </div>
