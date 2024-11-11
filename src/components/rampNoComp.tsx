6cedafdc4ecaf720ad13d60c5cd6f51923005f35
Sophie <sophie4140@outlook.com>
2024-11-11 13:49:40 +0800

    update RampNo component to retrieve URL from query parameters and adjust link accordingly

--- src/components/rampNoComp.tsx
+++ src/components/rampNoComp.tsx
@@ -1,7 +1,7 @@
 export const RampNo: React.FC = () => {
   const urlParams = new URLSearchParams(window.location.search);
-  const startapp = urlParams.get("rampNo");
-  const url = urlParams.get("env");
+  const url = urlParams.get("url");
+  const jumpURL = decodeURIComponent(url);
 
   return (
     <div className=" !h-[calc(100vh-50px)] flex flex-col  justify-between items-center">
@@ -9,7 +9,7 @@ export const RampNo: React.FC = () => {
       <a
         className="bg-[#F21F7F] w-[calc(100%-40px)] m-5 mb-8 rounded-lg px-4 py-2 text-white text-base text-center"
         target="_blank"
-        href={`${url}?startapp=RAMP_${startapp}`}
+        href={jumpURL}
       >
         Back to TOMO Telegram Wallet
       </a>
