209d3788d8f3654f74a4e5bb0008eaffb1059a90
tmac.lzh <jimmyli.remote@gmail.com>
2025-03-28 16:13:47 +0800

    feat: set assetsPrefix

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,31 @@ import image from "@astrojs/image";
 
 import react from "@astrojs/react";
 
+const ENV_MATRIX = {
+  development: {
+    site: "http://localhost:3000",
+    assetsPrefix: undefined
+  },
+  test: {
+    site: "https://preview.tomo.inc",
+    assetsPrefix: "https://preview.tomo.services"
+  },
+  production: {
+    site: "https://tomo.inc",
+    assetsPrefix: "https://tomo.services"
+  }
+};
+
+const env = process.env.NODE_ENV || 'development';
+const { site, assetsPrefix } = ENV_MATRIX[env];
+
 // https://astro.build/config
 export default defineConfig({
+  site,
+  base: '/',
+  build: {
+    assetsPrefix,
+  },
   devToolbar: {
     enabled: false,
   },
